045fffb51ae45d1d0935d9782323650af34736b0
Anskiy <dev221117@gmail.com>
2021-01-31 15:25:08 +0530

    Various stuff
    1. Added the right pointer for Yakra's spot.
    2. Accomodated Heckran's spot getting overloaded for certain bosses.
    3. Redid stats of unused bosses to make them fairer when placed into an active spot.

--- sourcefiles/bossrando.py
+++ sourcefiles/bossrando.py
@@ -18,69 +18,77 @@ import patcher as patch
 # Byte 5 - Charm Item
 # Byte 6 - TP
 
-# Bosses:  04 - Krawlie, 90 - Yakra, 95 - Golem, 99 - Masa & Mune, 9B - Nizbel, 9C - Nizbel II, 9D - Slash, 9E - Slash w/ Sword, 9F - Flea
+# Bosses:  04 - Krawlie, 90 - Yakra, 95 - Golem, 4F - Twin Golem, 99 - Masa & Mune, 9B - Nizbel, 9C - Nizbel II, 9D - Slash, 9E - Slash w/ Sword, 9F - Flea
 #          A1 - Dalton, A2 - Dalton Plus, A5 - Super Slash, A9 - Heckran, BB - Flea Plus, BD - Rust Tyrano, C0 - Atropus XR, C7 - Yakra XIII, F3 - Golem Boss
 
 # Yakra
 # Masa&Mune, Heckran
 # Slash, Flea, Golem, Twin Golem
 # Nizbel, Rust Tyrano, Yakra 13, Nizbel II
-spots = [0x1B38F0, 0x377824, 0x24EC52, 0x3ABF86, 0x1ED226, 0x1BEBBB, 0x38821C, 0x18FC30, 0x1B8A4C, 0x36F40B, 0x5FBBA]
+spots = [0x1B38C2, 0x377824, 0x24EC52, 0x3ABF86, 0x1ED226, 0x1BEBBB, 0x38821C, 0x18FC30, 0x1B8A4C, 0x36F40B, 0x5FBBA]
 spot_tiers = [0, 1, 1, 2, 2, 3, 3, 2, 2, 2, 2]
 
-# krawlie_spot = [500, 12, 5, 50, 40, 150, 100, 500, 5]
+#Order of stats: Pointer(where applicable), HP, Level, Magic, Magic Defense, Offense, Defense, XP, GP, TP
+#krawlie_spot = [500, 8, 5, 50, 44, 150, 100, 500, 5]
 #yakra_spot = [0x1B38C2, 920, 6, 9, 50, 16, 127, 200, 600, 5] # Changes:  50 -> 200 XP
-#golem_spot = [0x1BEBBB, 7000, 34, 22, 50, 105, 127, 1000, 2000, 35]
-#masamune_spot = [0x377824, 3600, 16, 6, 50, 40, 127, 500, 1500, 10]
-#nizbel_spot = [0x18FC30, 4200, 18, 33, 100, 60, 252, 500, 0, 10]
-#nizbel2_spot = [0x5FBBA, 6500, 25, 35, 50, 85, 127, 880, 0, 10]
-# slash_spot = [3200, 21, 6, 50, 40, 127, 0, 0, 0]
-#slashsword_spot = [0x3ABF86, 5200, 22, 7, 60, 70, 127, 880, 1500, 10]
-#flea_spot = [0x1ED226, 4120, 21, 10, 60, 45, 150, 880, 1000, 10]
-#dalton_spot = [3500, 32, 50, 50, 20, 127, 1000, 2500, 30]
-#daltonplus_spot = [2800, 32, 20, 50, 20, 127, 2500, 2000, 40]
-#superslash_spot = [2800, 37, 15, 50, 180, 127, 2500, 2000, 30]
-#heckran_spot = [0x24EC52, 2100, 13, 16, 50, 40, 253, 250, 1500, 10]
-# fleaplus_spot = [2500, 35, 15, 50, 120, 127, 1000, 0, 15]
-#rusttyrano_spot = [0x1B8A4C, 25000, 35, 30, 50, 1, 127, 3000, 3000, 40]
-#atropus_spot = [6000, 38, 10, 50, 38, 127, 0, 0, 0]
-#yakra13_spot = [0x36F40B, 18000, 45, 18, 50, 200, 127, 3500, 2000, 40]
-#golemboss_spot = [15000, 34, 18, 50, 40, 127, 2500, 2000, 40]
+#golem_spot = [0x1BEBBB, 6000, 15, 25, 50, 192, 127, 2500, 4000, 40]
+#twin_golem_spot = [0x38821C,7000,34,60,50,210,127,2000,5000,127]
+#masamune_spot = [0x377824, 3600, 10, 9, 50, 100, 127, 500, 1500, 10]
+#nizbel_spot = [0x18FC30, 5000, 10, 15, 55, 125, 253, 3000, 2200, 35]
+#nizbel2_spot = [0x5FBBA, 5400, 15, 20, 50, 135, 253, 3500, 3100, 46]
+#slash_spot = [4000, 10, 10, 60, 165, 127, 1400, 2100, 12]
+#slashsword_spot = [0x3ABF86, 4000, 10, 10, 60, 165, 127, 2800, 4200, 25]
+#flea_spot = [0x1ED226, 3800, 15, 15, 60, 100, 150, 2500, 4800, 30]
+#dalton_spot = [9000, 37, 33, 50, 200, 127, 3000, 1600, 50]
+#daltonplus_spot = [5000, 15, 12, 50, 75, 127, 2500, 2000, 20]
+#superslash_spot = [0x16FF0D, 2500, 35, 10, 50, 120, 127, 2000, 2000, 20]
+#heckran_spot = [0x24EC52, 2100, 13, 16, 50, 40, 253, 600, 1500, 10]
+#fleaplus_spot = [0x1BBE0D, 2500, 35, 15, 50, 120, 127, 2000, 2000, 20]
+#rusttyrano_spot = [0x1B8A4C, 5000, 16, 15, 50, 160, 127, 2400, 2000, 40]
+#atropos_XR_spot = [4000, 15, 15, 50, 100, 127, 2400, 2000, 40]
+#yakra13_spot = [0x36F40B, 4500, 17, 18, 50, 66, 127, 2200, 2000, 40]
+#golemboss_spot = [5000, 37, 37, 58, 210, 127, 2500, 2000, 40]
 
-# Yakra, Golem, Masa & Mune, Nizbel, Nizbel II, Slash w/ Sword, Flea, Dalton, Dalton Plus, Heckran, Super Slash, Flea Plus, RustTyrano, Atropos XR, Yakra XIII, Golem Boss
-eligible_bosses = [0x90, 0x95, 0x99, 0x9B, 0x9C, 0x9E, 0x9F, 0xA2, 0xA9, 0xBA, 0xBB, 0xBD, 0xC0, 0xC7, 0xF3]
-boss_tiers =      [   0,    2,    1,    2,    3,    2,    2,    3,    1,    2,    2,    3,    2,    3,    2]
+# Yakra, Golem, Golem Twins, Masa & Mune, Nizbel, Nizbel II, Slash, Slash w/ Sword, Flea, Dalton Plus, Heckran, Super Slash, Flea Plus, RustTyrano, Atropos XR, Yakra XIII, Golem Boss
+eligible_bosses = [0x90, 0x95, 0x4F, 0x99, 0x9B, 0x9C, 0x9E, 0x9F, 0xA2, 0xA9, 0xBA, 0xBB, 0xBD, 0xC0, 0xC7, 0xF3]
+boss_tiers =      [   0,    2,    3,    2,    2,    3,    1,    2,    3,    1,    2,    2,    3,    2,    3,    0]
 
 def randomize_bosses(outfile):
     # Reset array to initial position.  Program will crash if you don't do that because we remove elements from the array as bosses are selected.
-    eligible_bosses = [0x90, 0x95, 0x99, 0x9B, 0x9C, 0x9E, 0x9F, 0xA2, 0xA9, 0xBA, 0xBB, 0xBD, 0xC0, 0xC7, 0xF3]
-    boss_tiers =      [   0,    2,    1,    2,    3,    2,    2,    3,    1,    2,    2,    3,    2,    3,    2]
+    eligible_bosses = [0x90, 0x95, 0x4F, 0x99, 0x9B, 0x9C, 0x9E, 0x9F, 0xA2, 0xA9, 0xBA, 0xBB, 0xBD, 0xC0, 0xC7, 0xF3]
+    boss_tiers =      [   0,    2,    3,    2,    2,    3,    1,    2,    3,    1,    2,    2,    3,    2,    3,    0]
 
     f = open(outfile,"r+b")
     lnI = 0
     for spot in spots:
-        boss = rand.randrange(0,len(eligible_bosses),1)
-        boss_tier = boss_tiers[boss]
+        if spot == 0x24EC52: #Hack to get around sprite overload in Heckran's spot
+            safe_bosses =     [0x90, 0x95, 0x4F, 0x9B, 0x9C, 0xA2, 0xA9, 0xBB, 0xBD, 0xC7, 0xF3]
+            safe_boss_tiers = [0,    2,    3,    2,    3,    3,    1,    2,    3,    3,    0]
+            boss = rand.choice(safe_bosses)
+            boss_tier = safe_boss_tiers[safe_bosses.index(boss)]
+        else:
+            boss = rand.choice(eligible_bosses)
+            boss_tier = boss_tiers[eligible_bosses.index(boss)]
         spot_tier = spot_tiers[lnI]
         
-        f.seek(0xC4700 + (eligible_bosses[boss] * 23) + 0);
+        f.seek(0xC4700 + boss * 23 + 0);
         hp = int.from_bytes(f.read(2), byteorder='little', signed=False);
-        f.seek(0xC4700 + (eligible_bosses[boss] * 23) + 2);
+        f.seek(0xC4700 + boss * 23 + 2);
         level = int.from_bytes(f.read(1), byteorder='little', signed=False);
-        f.seek(0xC4700 + (eligible_bosses[boss] * 23) + 10);
+        f.seek(0xC4700 + boss * 23 + 10);
         magic = int.from_bytes(f.read(1), byteorder='little', signed=False);
         #f.seek(0xC4700 + (eligible_bosses[boss] * 23) + 13);
         #magic_defense = int.from_bytes(f.read(1), byteorder='little', signed=False);
-        f.seek(0xC4700 + (eligible_bosses[boss] * 23) + 14);
+        f.seek(0xC4700 + boss * 23 + 14);
         offense = int.from_bytes(f.read(1), byteorder='little', signed=False);
         #f.seek(0xC4700 + (eligible_bosses[boss] * 23) + 15);
         #defense = int.from_bytes(f.read(1), byteorder='little', signed=False);
 
-        f.seek(0xC5E00 + (eligible_bosses[boss] * 7) + 0);
+        f.seek(0xC5E00 + boss * 7 + 0);
         xp = int.from_bytes(f.read(2), byteorder='little', signed=False);
-        f.seek(0xC5E00 + (eligible_bosses[boss] * 7) + 2);
+        f.seek(0xC5E00 + boss * 7 + 2);
         gp = int.from_bytes(f.read(2), byteorder='little', signed=False);
-        f.seek(0xC5E00 + (eligible_bosses[boss] * 7) + 6);
+        f.seek(0xC5E00 + boss * 7 + 6);
         tp = int.from_bytes(f.read(1), byteorder='little', signed=False);
 
         boss_power = 1
@@ -106,33 +114,33 @@ def randomize_bosses(outfile):
         tp = min(int(pow(tp, boss_power)), 250)
 
         f.seek(spot)
-        f.write(st.pack("B",eligible_bosses[boss]))
+        f.write(st.pack("B",boss))
         # To avoid graphical glitches at the Masa & Mune spot
         if lnI == 1:
             f.seek(spot + 1)
             f.write(st.pack("B", 0x03))
 
-        f.seek(0xC4700 + (eligible_bosses[boss] * 23) + 0);
+        f.seek(0xC4700 + boss * 23 + 0);
         f.write(st.pack("<H",hp))
-        f.seek(0xC4700 + (eligible_bosses[boss] * 23) + 2);
+        f.seek(0xC4700 + boss * 23 + 2);
         f.write(st.pack("B",level))
-        f.seek(0xC4700 + (eligible_bosses[boss] * 23) + 10);
+        f.seek(0xC4700 + boss * 23 + 10);
         f.write(st.pack("B",magic))
         #f.seek(0xC4700 + (eligible_bosses[boss] * 23) + 13);
         #magic_defense = int.from_bytes(reader.read(1), byteorder='little', signed=False);
-        f.seek(0xC4700 + (eligible_bosses[boss] * 23) + 14);
+        f.seek(0xC4700 + boss * 23 + 14);
         f.write(st.pack("B",offense))
         #f.seek(0xC4700 + (eligible_bosses[boss] * 23) + 15);
         #defense = int.from_bytes(reader.read(1), byteorder='little', signed=False);
 
-        f.seek(0xC5E00 + (eligible_bosses[boss] * 7) + 0);
+        f.seek(0xC5E00 + boss * 7 + 0);
         f.write(st.pack("<H",xp))
-        f.seek(0xC5E00 + (eligible_bosses[boss] * 7) + 2);
+        f.seek(0xC5E00 + boss * 7 + 2);
         f.write(st.pack("<H",gp))
-        f.seek(0xC5E00 + (eligible_bosses[boss] * 7) + 6);
+        f.seek(0xC5E00 + boss * 7 + 6);
         f.write(st.pack("B",tp))
 
-        del eligible_bosses[boss]
-        del boss_tiers[boss]
-
+        boss_tiers.pop(eligible_bosses.index(boss))
+        eligible_bosses.remove(boss)
+        
         lnI = lnI + 1
