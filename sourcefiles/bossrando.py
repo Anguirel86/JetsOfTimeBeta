0342dc1a31cb44611cc77926c88fafd1cd944faf
Anskiy <dev221117@gmail.com>
2021-03-29 07:09:20 +0530

    Fixed early game scaling of XP and gold, capped them to not get out of hand

--- sourcefiles/bossrando.py
+++ sourcefiles/bossrando.py
@@ -123,8 +123,8 @@ def randomize_bosses(outfile):
         level = min(int(pow(level, boss_power)), 90)
         magic = min(int(pow(magic, boss_power)), 250)
         offense = min(int(pow(offense, boss_power)), 250)
-        xp = min(int(pow(xp, boss_power - 0.10)), 65000) #XP and gold scale up way too fast
-        gp = min(int(pow(gp, boss_power - 0.10)), 65000)
+        xp = min(int(pow(xp, boss_power)), 7000) #XP and gold scale up way too fast
+        gp = min(int(pow(gp, boss_power)), 12000)
         tp = min(int(pow(tp, boss_power)), 250)
 
         f.seek(spot)
